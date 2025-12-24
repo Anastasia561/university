cffd807537ee33a4740642c39657e0208b8a76b7
Anastasia561 <kurulenkoanastasia@gmail.com>
2025-12-24 22:00:23 +0100

    feat: add student delete logic

--- frontend/src/components/students/StudentsDelete.js
+++ frontend/src/components/students/StudentsDelete.js
@@ -1,27 +1,64 @@
-import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from 'react-router-dom';
 import '../../styles/ConfirmStyles.css';
 
 function StudentsDelete() {
+    const {id} = useParams();
     const navigate = useNavigate();
 
-    const handleDelete = (e) => {
+    const [student, setStudent] = useState(null);
+    const [serverMessage, setServerMessage] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const handleDelete = async (e) => {
         e.preventDefault();
-        //api call
-        alert('Student deleted!');
-        navigate('/students');
+        try {
+            const res = await fetch(`/api/students/${id}`, {
+                method: 'DELETE',
+            });
+            if (!res.ok) {
+                const data = await res.json();
+                setServerMessage(data.message);
+            } else {
+                navigate('/students');
+            }
+        } catch (err) {
+            console.log(err);
+        }
     };
+
+    useEffect(() => {
+        const fetchStudent = async () => {
+            try {
+                const res = await fetch(`/api/students/details/${id}`);
+                const data = await res.json();
+                if (!res.ok) {
+                    setServerMessage(data.message);
+                }
+                setStudent(data);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchStudent();
+    }, [id]);
+
+    if (loading) return <p>Loading student data...</p>;
 
     return (
         <div className="confirm-box">
             <h2>Confirm Deletion</h2>
             <p>Are you sure you want to delete this student?</p>
 
+            {serverMessage && <div className="error general-error">{serverMessage}</div>}
+
             <ul>
-                <li><strong>First Name:</strong> Anna</li>
-                <li><strong>Last Name:</strong> Kowalska</li>
-                <li><strong>Email:</strong> anna.kowalska@example.com</li>
-                <li><strong>Birth Date:</strong> 2001-05-12</li>
+                <li><strong>First Name: </strong>{student.firstName}</li>
+                <li><strong>Last Name: </strong>{student.lastName}</li>
+                <li><strong>Email: </strong>{student.email}</li>
+                <li><strong>Birth Date: </strong>{student.birthdate}</li>
             </ul>
 
             <div>
